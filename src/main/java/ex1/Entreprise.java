250cf896ddd66a77195081f813d68abd9ee155a9
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-10-10 11:17:07 +0200

    ex01 entreprise

---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,34 @@
+package ex1;
+
+import java.util.Date;
+
+public class Entreprise {
+
+	/** Siret : int  */
+	  
+	public int Siret;
+	
+	/** Nom : String  */
+	  
+	public String Nom;
+	
+	/** adresse : String  */
+	  
+	public String adresse;
+	
+	/** date_Creation : Date  */
+	  
+	public Date date_Creation;
+	
+	/** capitalMax : int  */
+	  
+	public static final int capitalMax = 3000000;
+	
+	/**
+	 * 
+	 */
+	public void afficher_statut(){
+		
+	}
+	
+}
