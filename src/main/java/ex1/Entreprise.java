4edc3f9c92fda3fd462900051017959d3b9a92d6
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-10-10 11:29:51 +0200

    getter setters pas finis

--- src/main/java/ex1/Entreprise.java
+++ src/main/java/ex1/Entreprise.java
@@ -2,33 +2,74 @@ package ex1;
 
 import java.util.Date;
 
+
+/** Représente une entreprise
+ * @author ETY8
+ *
+ */
 public class Entreprise {
 
-	/** Siret : int  */
-	  
-	public int Siret;
+	/** Siret : int  */  
+	private int siret;
 	
-	/** Nom : String  */
-	  
-	public String Nom;
+	/** Nom : String  */  
+	private String nom;
 	
-	/** adresse : String  */
-	  
-	public String adresse;
+	/** adresse : String  */  
+	private String adresse;
 	
-	/** date_Creation : Date  */
-	  
-	public Date date_Creation;
+	/** dateCreation : Date  */ 
+	private Date dateCreation;
 	
-	/** capitalMax : int  */
+	
 	  
-	public static final int capitalMax = 3000000;
+	
+	/** CAPITAL_MAX : int -> représente le capital maximum d'une entreprise */
+	  
+	public static final int CAPITAL_MAX = 3000000;
 	
 	/**
-	 * 
+	 affiche le statut
 	 */
-	public void afficher_statut(){
+	public void afficherstatut(){
 		
 	}
+
+	public int getSiret() {
+		return siret;
+	}
+
+	public void setSiret(int siret) {
+		this.siret = siret;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	
+
+
+	
 	
 }
--- src/main/java/notes.java
+++ src/main/java/notes.java
@@ -13,3 +13,13 @@ public class notes {
 //javadoc raccourci = ctrl + alt + j
 
 //windows -> preferences -> javadoc -> code templates -> comments -> fields -> /** ${field} : ${field_type}  */
+
+// Pour automatiser les getters / setters
+
+/** Getter pour ${field}
+* @return the ${bare_field_name}
+*/
+
+/** Setter pour ${field}
+ * @return the ${bare_field_name}
+ */
