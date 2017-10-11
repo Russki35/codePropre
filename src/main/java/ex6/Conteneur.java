522b4769082f4f5c4458703bcb64468bc4742b41
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-10-11 11:25:32 +0200

    ex 06 corrigé

--- src/main/java/ex6/Conteneur.java
+++ src/main/java/ex6/Conteneur.java
@@ -3,30 +3,38 @@ package ex6;
 public class Conteneur {
 
 	private boolean readOnly;
-	
+
 	private Object[] elements;
 	private int size;
-	
-	public void add(Object element){
-		if (!readOnly){
-			int newSize = size+1;
-			if (newSize > size){
-				Object[] newElements = new Object[size+10];
-				for (int i=0; i<size; i++){
-					newElements[i] = elements[i];
-				}
-				elements = newElements;
-			}
-			elements[size++]=element;
+
+	public void add(Object element) {
+		if (readOnly) {
+			return;
 		}
-		
+		int newSize = size + 1;
+		if (newSize > size) {
+			agrandirTab();
+		}
+		elements[size++] = element;
+
 	}
-	
-	public Object getContent(int i){
+
+	/**
+	 * 
+	 */
+	private void agrandirTab() {
+		Object[] newElements = new Object[size + 10];
+		for (int i = 0; i < size; i++) {
+			newElements[i] = elements[i];
+		}
+		elements = newElements;
+	}
+
+	public Object getContent(int i) {
 		return elements[i];
 	}
-	
-	public int getSize(){
+
+	public int getSize() {
 		return size;
 	}
 }
